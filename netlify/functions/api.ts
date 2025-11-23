e6e776b9234cc84238ab75241576d231ebe26108
Yash Kabra <yashkabra143@gmail.com>
2025-11-24 00:43:33 +0530

    Migration: Switch from Netlify to Vercel

--- netlify/functions/api.ts
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import { app } from "../../server/app";
-import { registerRoutes } from "../../server/routes";
-
-// Initialize routes
-registerRoutes(app);
-
-export const handler = serverless(app);
